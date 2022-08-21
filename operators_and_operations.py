65362b85f084ff074e1bcc54b295f5c07e55fa79
nitesh038 <nitesh.singh038@gmail.com>
2022-08-21 21:19:20 +0530

    basics of python

---
+++ operators_and_operations.py
@@ -0,0 +1,13 @@
+########## Operators and Mathematical Operations ############3
+
+print (3+5)
+#python follows BODMAS
+print (1 + 2 + 3 * 3)
+#exponentiation
+print (3**2)
+#modulo - remainder
+print ( 9 % 2)
+# // - integer division
+print ( 7 // 2) # rounds down to 3
+print ( -7 // 2) # rounds to -4
+
---
+++ variables_operators_and_data_types.py
@@ -0,0 +1,12 @@
+# here population is variable
+population = 10
+
+print (population)
+
+# multiple assignments - use this to defing closely related variables like width and height or closely related
+# coordinates etc.
+x, y, z = 2, 3, 5
+
+print(x)
+print(y)
+print(z)
